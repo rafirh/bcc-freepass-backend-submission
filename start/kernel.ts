bb8a309f7e4756026733c0c906fb19b8e945f44e
rafirh <rafirahmann18@gmail.com>
2026-02-05 21:09:50 +0700

    fix: Remove unused app import from kernel.ts

--- start/kernel.ts
+++ start/kernel.ts
@@ -10,7 +10,6 @@
 
 import router from '@adonisjs/core/services/router'
 import server from '@adonisjs/core/services/server'
-import app from '@adonisjs/core/services/app'
 
 /**
  * The error handler is used to convert an exception
